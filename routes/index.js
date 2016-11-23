643b997384d9a60b3b2702206ab4b1f18e23b337
tarekkhoury <tarekkhoury78@gmail.com>
2016-11-24 00:05:29 +1100

    nodemon commit

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,18 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+
+// just a test api with id param
+router.get('/api/v1.0/test/:id', function(req, res, next) {
+	res.json([{
+		"test": "OK",
+		"id": req.params.id
+	}, {
+		"test": "OK2",
+		"id": req.params.id
+	}]);
+});
+
+
 module.exports = router;
